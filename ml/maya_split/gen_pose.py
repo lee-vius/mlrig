c8a8677a85ea4a93dd204c6e32f5df6dfa7ca071
lee-vius <leevius@umich.edu>
2020-12-10 14:43:27 +0800

    try a function

--- ml/maya_split/gen_pose.py
+++ ml/maya_split/gen_pose.py
@@ -442,6 +442,83 @@ def setBlendShapeData(node,
             mc.setAttr(shapeAttr + ".targetWeights[" + str(weight) + "]", targetWeights[weight])
 
 
+def setShapeData(node,
+                 shapeData,
+                 inputIndex=0,
+                 shapes=None):
+    """
+    sets the shape data onto a blendShape node.
+    :param str node: the blendShape to add the shapes to.
+    :param dict shapeData: the shape data to apply to the node.
+    :param list shapes: if given, only modify given target names
+    """
+    nodeType = mc.nodeType(node)
+    inputShape = mc.deformer(node, g=True, q=True)
+    shapeAliasLookup = getShapeAliasLookup(node)
+
+    if not 'shapes' in shapeData:
+        print("procedureName" + ':  shapeData does not have a "shapes" key.  Returning now...')
+        return
+
+    for shapeAlias in shapeData['shapes']:
+        if shapes and shapeAlias not in shapes:
+            continue
+
+        # read the information stored for this shape
+        targetData = shapeData['shapes'][shapeAlias]
+        targetOffsets = targetData["offsets"]
+        targetWeights = targetData["weights"]
+        shapeIndex = shapeAliasLookup.get(shapeAlias, None)
+
+        # if the shape doesn't already exist, create it at the end of the list
+        newShape = False
+        if shapeIndex is None:
+            newShape = True
+            weightIndices = mc.getAttr(node + ".weight", mi=True)
+            if weightIndices is None:
+                shapeIndex = 0
+            else:
+                shapeIndex = weightIndices[-1] + 1
+
+            mc.addMultiInstance(node + '.weight[' + str(shapeIndex) + ']')
+            if shapeAlias[0] != '[':
+                mc.aliasAttr(shapeAlias.strip(), node + ".weight[" + str(shapeIndex) + "]")
+
+        # iterate through the offset dictionary
+        pointList = []
+        componentList = []
+        #speed optimization
+        shapeComponentsToUse = {}
+        
+        for pointIndex in targetOffsets:
+            pointData = targetOffsets[pointIndex]                       
+            pointList.append((pointData[0], pointData[1], pointData[2], 1.0))
+            componentList.append('vtx[' + str(pointIndex) + ']')
+        
+        # create the element by calling getAttr
+        try:
+            mc.getAttr(node + '.inputTarget[' + str(inputIndex) + ']')
+        except:
+            pass
+        try:
+            mc.getAttr(node + '.inputTarget[' + str(inputIndex) + '].inputTargetGroup[' + str(shapeIndex) + ']')
+        except:
+            pass
+
+        shapeAttr = node + ".inputTarget[" + str(inputIndex) + "].inputTargetGroup[" + str(shapeIndex) + "]"
+        mc.setAttr(shapeAttr + ".inputTargetItem[6000].inputPointsTarget", len(componentList), type="pointArray", *pointList)
+        mc.setAttr(shapeAttr + ".inputTargetItem[6000].inputComponentsTarget", len(componentList), type="componentList", *componentList)
+
+        tAttrs = mc.listAttr(shapeAttr, m=True, string='targetWeights')
+        if tAttrs != None:
+            for a in tAttrs:
+                mc.removeMultiInstance((node + '.' + a), b=True)
+        # set the weights
+        for weight in targetWeights:
+            mc.setAttr(shapeAttr + ".targetWeights[" + str(weight) + "]", targetWeights[weight])
+
+
+
 def getShapeAliasLookup(node):
     """
     Builds a lookup dictionary that maps a blendShape node's weight
@@ -560,7 +637,7 @@ def reconstruction(training_type='differential', ground_truth=False):
         for vtx_id in cur_offset_data:
             shape_data['shapes'][TEMP_TARGET]['offsets'][vtx_id] = cur_offset_data[vtx_id]
 
-    setBlendShapeData(BLENDSHAPE, shape_data, shapes=[TEMP_TARGET])
+    setShapeData(BLENDSHAPE, shape_data, shapes=[TEMP_TARGET])
     mc.setAttr(BLENDSHAPE + '.' + TEMP_TARGET, 1.0)
 
 
